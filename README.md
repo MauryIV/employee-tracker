efa533ec916c8c6d90ad1359d36357f8571d0b25
MauryIV <mauryhughesiv@gmail.com>
2024-03-17 21:20:04 -0600

    finished the 'viewing' results user inquirer

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-WHEN I choose to view all roles
-THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-<!-- after choosing view all roles, formatted table with job title, role id, the department that role belongs to, and salary for role. -->
 WHEN I choose to view all employees
 THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 <!-- after choosing view all employees, formatted able with employee ids, first and last name, job title, department, salary, and their manager -->
--- assets/db-functions.js
+++ assets/db-functions.js
@@ -12,7 +12,10 @@ const pool = new Pool (
 );
 
 function departments() {
-  const getSql = 'SELECT department.dept_name AS "Department", id AS "Department ID" FROM department ORDER BY department.dept_name';
+  const getSql = `SELECT department.dept_name AS "Department", 
+id AS "Department ID" 
+FROM department 
+ORDER BY department.dept_name`;
   pool.query(getSql, (err, result) => {
     const table = new Table({
       head: ['Department', 'Department ID']
@@ -29,10 +32,54 @@ function departments() {
 };
 
 function roles() {
-
+  const getSql = `SELECT role.title AS "Title", 
+role.id AS "Title ID", 
+department.dept_name AS "Department", 
+role.salary AS "Salary" 
+FROM role 
+LEFT JOIN department ON department.id = role.dept_id 
+ORDER BY department.dept_name ASC, role.title ASC`;
+  pool.query(getSql, (err, result) => {
+    const table = new Table({
+      head: ['Title', 'Title ID', 'Department', 'Salary']
+    });
+    if (err) {
+      console.error('Having this issue: ', err);
+    } else {
+      result.rows.forEach(row => {
+        table.push([row['Title'], row['Title ID'], row['Department'], row['Salary']]);
+      });
+      console.log(table.toString());
+    }
+  });
 };
-function employees() {
 
+function employees() {
+  const getSql = `SELECT employee.id AS "Employee ID", 
+employee.first_name AS "First Name", 
+employee.last_name AS "Last Name", 
+department.dept_name AS "Department", 
+role.title AS "Title", 
+role.salary AS "Salary", 
+employee.manager_id AS "Manager ID" 
+FROM employee 
+LEFT JOIN role ON role.id = employee.role_id 
+LEFT JOIN department ON department.id = role.dept_id 
+ORDER BY department.dept_name ASC, employee.last_name ASC`;
+  pool.query(getSql, (err, result) => {
+    const table = new Table({
+      head: ['Employee ID', 'First Name', 'Last Name', 'Department', 'Title', 'Salary', 'Manager ID']
+    });
+    if (err) {
+      console.error('Having this issue: ', err);
+    } else {
+      result.rows.forEach(row => {
+        const managerID = row['Manager ID'] ? row['Manager ID'] : '';
+        table.push([row['Employee ID'], row['First Name'], row['Last Name'], row['Department'], row['Title'], row['Salary'], managerID]);
+      });
+      console.log(table.toString());
+    }
+  });
 };
 
 function addDepartment() {
